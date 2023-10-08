b2bc06eb53c0744252c662dbfadb02e62c7a6d19
MichaelSteedle <146889350+MichaelSteedle@users.noreply.github.com>
2023-10-08 12:33:52 -0400

    Rule 17 - Noncompliant Code

---
+++ R17_JNI01_J.java
@@ -0,0 +1,32 @@
+/* JNI01-J. Safely invoke standard APIs that perform tasks using the immediate caller's class loader instance (loadLibrary) */
+
+// Trusted.java
+ 
+import java.security.*;
+ 
+public class Trusted {
+ 
+   public static void loadLibrary(final String library){
+      AccessController.doPrivileged(new PrivilegedAction<Void>() {
+         public Void run() {
+             System.loadLibrary(library);
+             return null;
+         }
+      });
+   }
+}
+ 
+---------------------------------------------------------------------------------
+ 
+// Untrusted.java
+ 
+public class Untrusted {
+ 
+   private native void nativeOperation();
+ 
+   public static void main(String[] args) {
+      String library = new String("NativeMethodLib");
+      Trusted.loadLibrary(library);
+      new Untrusted.nativeOperation();  // invoke the native method
+   }
+}
